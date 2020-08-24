e3303e1dd86e5a98dc0f72286b4132f34df101a6
Luis Felipe <luisFAOSetecH@gmail.com>
2020-08-24 13:06:22 -0300

    Revert "testando rotas"
    This reverts commit 87c1aa1c657b1a27865ecfb13ec708020eb4bc07.

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import { AppLoading } from 'expo';
-
-import MealsNavigator from './navigation/MealsNavigator';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
   return (
-    <AppLoading
-      startAsync={fetchFonts}
-      onFinish={() => setFontLoaded(true)}
-    />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-
-  return <MealsNavigator />;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
